04d8993d60516c1b0d5aa0a837fdaad9ac4881eb
Charlie Gleason <cgleason@salesforce.com>
2023-01-24 18:24:56 +0000

    Added new badge

--- app/components/ui/events/item.tsx
+++ app/components/ui/events/item.tsx
@@ -1,5 +1,5 @@
 import Link from '~/components/ui/link'
-import Date from '~/components/ui/events/date'
+import DateComponent from '~/components/ui/events/date'
 
 type Props = {
   date?: string
@@ -9,14 +9,30 @@ type Props = {
     href: string
   }
   stack?: boolean
+  added?: string
 }
 
-export default function Item({ date, title, link, stack }: Props) {
+export default function Item({ date, title, link, stack, added }: Props) {
   const classes = stack ? 'flex flex-col' : 'flex'
+
+  if (added && (!parseInt(added) || new Date(added).getTime() <= 0)) {
+    console.warn('Incorrect date provided—use Date.now()')
+    return null
+  }
+
+  const highlight = added
+    ? Math.round((Date.now() - parseInt(added)) / (1000 * 60 * 60 * 24)) <= 14
+    : null
+
   return (
     <li className={`${classes} gap-2`}>
-      {date && !stack && <Date date={date} />}
+      {date && !stack && <DateComponent date={date} />}
       <div>
+        {highlight && (
+          <span className="relative -ml-1 -top-[1px] border border-yellow-600 text-yellow-600 dark:border-yellow-700 dark:text-yellow-500 rounded-md px-1 py-0.25 mr-1 leading-none font-display text-xxs">
+            New
+          </span>
+        )}
         {title}
         {link && (
           <>
@@ -26,7 +42,7 @@ export default function Item({ date, title, link, stack }: Props) {
         {date && stack && (
           <>
             <br />
-            <Date date={date} />
+            <DateComponent date={date} />
           </>
         )}
       </div>
