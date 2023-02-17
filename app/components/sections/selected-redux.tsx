812bbd7d9973c96ed2110755233b9981101aa332
Charlie Gleason <cgleason@salesforce.com>
2023-02-17 18:15:01 +0000

    Added images

--- app/components/sections/selected-redux.tsx
+++ app/components/sections/selected-redux.tsx
@@ -28,39 +28,43 @@ type Note = {
 }
 
 export default function Selected({ sections }: Props) {
-  return sections.map((section) => (
-    <div key={section.title} className="space-y-8">
-      <Title>Selected {section.title}</Title>
-      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-y-12 gap-x-4 md:gap-x-12">
-        {section.data.map((entry) => {
-          return (
-            <div key={entry.title} className="flex items-start">
-              <img
-                className="relative mr-2 md:mr-4 rounded-full w-[24px] h-[24px] md:w-[30px] md:h-[30px]"
-                src={`${EMOJI_URL}${entry.icon}?detailed=false&animated=false`}
-                alt={`${entry.icon} emoji in a yellow circle`}
-                width="30"
-                height="30"
-              />
-              <div>
-                {entry.property && (
-                  <div className="text-xs uppercase tracking-widest font-medium text-neutral-700 dark:text-neutral-400">
-                    {entry.property}
+  return (
+    <>
+      {sections.map((section) => (
+        <div key={section.title} className="space-y-8">
+          <Title>Selected {section.title}</Title>
+          <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-y-12 gap-x-4 md:gap-x-12">
+            {section.data.map((entry) => {
+              return (
+                <div key={entry.title} className="flex items-start">
+                  <img
+                    className="relative mr-2 md:mr-4 rounded-full w-[24px] h-[24px] md:w-[30px] md:h-[30px]"
+                    src={`${EMOJI_URL}${entry.icon}?detailed=false&animated=false`}
+                    alt={`${entry.icon} emoji in a yellow circle`}
+                    width="30"
+                    height="30"
+                  />
+                  <div>
+                    {entry.property && (
+                      <div className="text-xs uppercase tracking-widest font-medium text-neutral-700 dark:text-neutral-400">
+                        {entry.property}
+                      </div>
+                    )}
+                    <Link href={entry.url} size="large">
+                      {entry.title}
+                    </Link>
+                    {entry.subtitle && (
+                      <div className="text-sm text-neutral-700 dark:text-neutral-400">
+                        {entry.subtitle}
+                      </div>
+                    )}
                   </div>
-                )}
-                <Link href={entry.url} size="large">
-                  {entry.title}
-                </Link>
-                {entry.subtitle && (
-                  <div className="text-sm text-neutral-700 dark:text-neutral-400">
-                    {entry.subtitle}
-                  </div>
-                )}
-              </div>
-            </div>
-          )
-        })}
-      </div>
-    </div>
-  ))
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      ))}
+    </>
+  )
 }
