878ea206243456cd23d75dfdf96d9105a4af588e
Charlie Gleason <cgleason@salesforce.com>
2023-02-27 11:21:16 +0000

    Fixed spacing

--- app/routes/emoji/$emoji.tsx
+++ app/routes/emoji/$emoji.tsx
@@ -305,8 +305,8 @@ export async function loader({ params, request }: any) {
   const detailed = url.searchParams.get('detailed') !== 'false'
 
   const pre = `
-<svg width="80" height="80" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="50%" cy="50%" r="${detailed ? '50%' : '45%'}" fill="#fbe047" />`
+    <svg width="80" height="80" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="50%" cy="50%" r="${detailed ? '50%' : '45%'}" fill="#fbe047" />`
 
   let { readable, writable } = new TransformStream()
   const writer = writable.getWriter()
